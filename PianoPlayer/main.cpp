3cebd8e1acae58b6630ba5657a656fa8a11cec9a
Ben Smith <bdsmith3@illinois.edu>
2011-09-28 09:56:38 -0500

    Initial Commit

---
+++ PianoPlayer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PianoPlayer
+//
+//  Created by Ben Smith on 9/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
